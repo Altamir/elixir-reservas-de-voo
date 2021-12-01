53c83beb4685b509ace5c204e3124560e6e7b256
Altamir Benkenstein <altamir.benkenstein@gmail.com>
2021-12-01 12:59:22 -0300

    Add start_agents

--- lib/flightex.ex
+++ lib/flightex.ex
@@ -1,3 +1,12 @@
 defmodule Flightex do
+  alias Flightex.Bookings.Agent, as: BookinAgent
+  alias Flightex.Users.Agent, as: UserAgent
+
+  alias Flightex.Bookings.CreateOrUpdate, as: CreateOrUpdateBooking
+
+  def start_agents do
+    UserAgent.start_link(%{})
+    BookinAgent.start_link(%{})
+  end
 
 end
